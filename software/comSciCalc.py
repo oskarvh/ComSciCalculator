370dee814f2a7c58ba38cca02c232b96f3bc5f90
Oskar vH <oskar.von.heideken@gmail.com>
2023-02-26 16:12:37 -0800

    Decimal result parsing and calculation using eval working. String coloring depending on brackets working

--- software/comSciCalc.py
+++ software/comSciCalc.py
@@ -39,8 +39,23 @@ class Brackets:
         self.end = end
         self.color = color
 
-# Custom text class to handle the input text widget. 
-# This is becoming very Frankensteined, I most likely do not need it. 
+# Class to handle any arbitrary calculation
+class subresult:
+    # A list of other subresults that needs to be calculated before this one
+    # This is in itself a list of subresult, i.e. this class. 
+    subRes = []
+    # substring of which the subres are:
+    substring = ""
+    # The operator on which the subresults of this should act on
+    operator = None
+    # Once the result of the operation on these subresults
+    result = None
+    # A flag to show if this result is solved. 
+    solved = False
+    # 
+
+
+# Class to customize the text output. 
 class CustomText:
     colorWheel = ["white", "deep sky blue", "cyan", "green", "yellow", "pink", "purple3"]
     colorWheelIndex = 0
@@ -48,12 +63,25 @@ class CustomText:
     inputTextWidget = None
     statusWidget = None
     inputString = ""
-    
-    def __init__(self, inputWidget, inputWidgetName):
+    decResultString = ""
+    hexResultString = ""
+    floatResultString = ""
+    binResultString = ""
+    hexOutputWidget = None
+    decOutputWidget = None
+    floatOutputWidget = None
+    binOutputWidget = None
+    calcResult = subresult
+
+    def __init__(self, inputWidget, inputWidgetName, hexOutputWidget, decOutputWidget, floatOutputWidget, binOutputWidget):
         self.inputMode = "Decimal"
         self.inputTextWidget = inputWidget
         self.statusWidget = inputWidgetName
         self.statusWidget.config(text="Input mode: "+self.inputMode)
+        self.hexOutputWidget = hexOutputWidget
+        self.decOutputWidget = decOutputWidget
+        self.floatOutputWidget = floatOutputWidget
+        self.binOutputWidget = binOutputWidget
 
     # This function gets the text from the input and places it in the queue
     def inputText(self, event):
@@ -86,7 +114,7 @@ class CustomText:
         # Update the display once it's done. 
         self.displayText()
 
-    # This function updates the display
+    # This function updates the input display
     def displayText(self):
         # First, enable the state again in order to write to it:
         self.inputTextWidget.config(state=tk.NORMAL)
@@ -129,15 +157,87 @@ class CustomText:
 
         index = 0
         for bracket in reversed(formattingTags):
-            logger.debug("start = " + bracket.start)
-            logger.debug("end = " + bracket.end)
-            logger.debug("Color = " + bracket.color)
-            logger.debug(" ")
             self.inputTextWidget.tag_add(str(index), bracket.start, bracket.end)
             self.inputTextWidget.tag_config(str(index), background=backGroundColor, foreground=bracket.color)
             index += 1
         # Disable the widget so that no one can write to it. 
         self.inputTextWidget.config(state=tk.DISABLED)
+        self.stringInterpreter()
+
+    # Function to handle the input
+    # The idea has to be to divide the input into numbers and operators. 
+    # 
+    def stringInterpreter(self):
+        # Test to evaulate this as a python statement. Probably not a good idea. 
+        # It's better to write a parser. 
+        self.decResultString = str(eval(self.inputString))
+        logger.debug("result = " + self.decResultString)
+
+        # All calculations should be done in one base, and then translated into each result
+        # That way we only need one calculation part. 
+        # The input can however be any type, so a translation of each type is needed first. 
+        
+        # Maybe a way to do it is to divide the string into several substrings, such
+        # that the operations that needs to be carried out first are so. 
+        # The priority would be to handle all the parentheses first, and we have
+        # already a function for that. Then, it would be mulitplication, division and lastly 
+        # addition/subtration. 
+        # An example would be 5+10*5/4-5*6or7: 10*5 and 6or7 would have the highest prio, 
+        # followed by (10*5)/4, and then lastly the addition and subtraction. 
+        # So, an interpreter would be useful in order to make this. 
+        # 
+        # One way to solve this could be to use a recursive operator which would go through
+        # a list of operations that then can be solved. 
+        # I.e. basically a list of pointers as follows:
+        # the entry would be the lowest priority tasks, such as + and -, and any
+        # other operations that require maths is then pointed to by that. 
+        # Basically, this would be a tree. 
+        # The tree would consist of several branches per node, with each depth of the tree could 
+        # correspond to an operation, such that if level 1 is addition, level 2 is subtraction, 
+        # level 3 is division, level 4 is multiplication etc. 
+        # This can further be simplified using signed math for additon, removing multiplication, 
+        # and a built in funciton for inversion to simplify division. 
+        #
+        # A graphical representation can be seen below. 
+        # 
+        #                   result
+        #                  / + \          ... \ 
+        #      subresult1     subresult2 ... subresult_n
+        #         /    \           /  \           /     \ 
+        # subsubres1 subsubres2 ssres3 ssres4 ssres_k-1 ssres_k
+        # ...
+        # 
+        # That way, each string can be interpreted as a single operation, and nested operations
+        # would just point to a result deeper in the tree. 
+        # That way, the interpreter would organize this tree from a single input string, 
+        # and the function to interpret each result and subsresult would be a single function
+        # with only two args. 
+        # Below are trees for the cases (a): 1+2+3, (b):1+2+(3+1), (c):2*(4+1), (d): 2*2+3
+        # 
+        # The structure of such a list would have to contain an operator on which to act on the 
+        # subresults, a list of subresults (and the length), and if it has been solved. 
+        # The operator could well be a function pointer as well. The function pointer would 
+        # have to support an arbitrary number of arguments though. 
+        # 
+        # If a statement is complete and solvable, the interpreter should invoke the 
+        # solver, which will iterate through the subresults and display them. 
+        # The task of this interpreter is therefore to go through the results, divide them into
+        # subgroups (i.e. subresults), check the integrity of the string and call the solver
+
+        # We need to loop through the string
+        lenOfInputString = len(self.inputString)
+        for inputStringIndex in range(0, lenOfInputString):
+            currentChar = self.inputString(inputStringIndex)
+            # Get the current character
+            if self.inputMode is "Decimal":
+                if currentChar in range(0,9):
+                    # Check if the subresult is empty:
+                    if empty(self.calcResult.subres):
+                        self.calcResult.subres.append(subres())
+
+
+
+
 
 
 
@@ -185,9 +285,6 @@ root.config(bg=backGroundColor) #26242f
 # We need one input window and three output windows:
 inputWidgetName = Label(text = "Input", fg=foreGroundColor, bg=backGroundColor, font="Courier 10")
 inputWidget = Text(root, bg = backGroundColor, fg = foreGroundColor, font="Courier 15")
-#inputWidget = CustomText(root, bg = backGroundColor, fg = foreGroundColor, font="Courier 15")
-#inputWidget.bind("<<TextModified>>",inputWidget.getText)
-#inputWidget.config(fg="white")
 inputWidget.config(state=DISABLED)
 # hex widget:
 hexWidgetName = Label(text = "Hex", fg=foreGroundColor, bg=backGroundColor, font="Courier 10")
@@ -222,7 +319,7 @@ root.rowconfigure(1, weight=1)
 
 
 # re-bind the keypress:
-inputHandler = CustomText(inputWidget, inputWidgetName)
+inputHandler = CustomText(inputWidget, inputWidgetName, hexWidget, decWidget, floatWidget, binWidget)
 root.bind("<KeyPress>", inputHandler.inputText)
 
 # Start the GUI main loop
