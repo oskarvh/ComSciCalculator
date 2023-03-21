11b9eb272b8d3be9bf56d3657e06c4ff63afeb06
Oskar vH <oskar.von.heideken@gmail.com>
2023-03-20 18:31:33 -0700

    Adding unity test framework

---
+++ software/run_unit_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Script to setup git repos and other things needed for this repo. 
+
+# Setup software. 
+echo "Compiling source code\r\n"
+
+echo "Compiling tests\r\n"
+
+echo "Running tests\r\n"
