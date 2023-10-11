f20a0f0f33f54e12c6cf4f6ff02d75f942198177
Oskar vH <oskar.von.heideken@gmail.com>
2023-10-10 22:37:02 -0700

    First build of renesas blinky using cmake

---
+++ firmware/renesas/config.cmake
@@ -0,0 +1,5 @@
+# Configuration file for user settings
+
+# This variable holds the path to the compiler. 
+# NOTE, for windows this needs to point to the <compiler>/bin path.
+# set(CMAKE_FIND_ROOT_PATH "<set path to the GCC arm renesas compiler here>")
---
+++ firmware/renesas/toolchain/gcc-arm-none-eabi.cmake
@@ -0,0 +1,50 @@
+# Cross compilation system information
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+# Include the config file which specifies where the compiler lives
+include(${CMAKE_CURRENT_LIST_DIR}/../config.cmake)
+
+if (NOT CMAKE_FIND_ROOT_PATH)
+  message(FATAL_ERROR "Toolchain path not defined. Please set CMAKE_FIND_ROOT_PATH variable in Config.cmake")
+endif()
+
+# Switch for Linux or windows
+if (CMAKE_HOST_WIN32 )
+  SET(BINARY_FILE_EXT ".exe")
+else()
+  SET(BINARY_FILE_EXT "")
+endif()
+
+# Name prefix of the compiler itself
+set(TOOLCHAIN_PREFIX arm-none-eabi)
+
+# Compile to static library 
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+# Set the path and name of the C, CPP and ASM compilers
+set(CMAKE_C_COMPILER ${CMAKE_FIND_ROOT_PATH}/${TOOLCHAIN_PREFIX}-gcc${BINARY_FILE_EXT})
+SET(CMAKE_C_COMPILER_ID CCRX)
+SET(CMAKE_C_COMPILER_ID_RUN TRUE)
+SET(CMAKE_C_COMPILER_FORCED TRUE)
+set(CMAKE_CXX_COMPILER ${CMAKE_FIND_ROOT_PATH}/${TOOLCHAIN_PREFIX}-g++${BINARY_FILE_EXT})
+SET(CMAKE_CXX_COMPILER_ID CCRX)
+SET(CMAKE_CXX_COMPILER_ID_RUN TRUE)
+SET(CMAKE_CXX_COMPILER_FORCED TRUE)
+set(CMAKE_ASM_COMPILER ${CMAKE_FIND_ROOT_PATH}/${TOOLCHAIN_PREFIX}-gcc${BINARY_FILE_EXT})
+SET(CMAKE_ASM_COMPILER_FORCED TRUE)
+
+# Specify the linker
+set(CMAKE_LINKER ${CMAKE_FIND_ROOT_PATH}/${TOOLCHAIN_PREFIX}-gcc${BINARY_FILE_EXT})
+
+# Set archiving tool, object copy and object dump
+#set(CMAKE_AR ${CMAKE_FIND_ROOT_PATH}/${TOOLCHAIN_PREFIX}-ar${BINARY_FILE_EXT})
+#set(CMAKE_OBJCOPY ${CMAKE_FIND_ROOT_PATH}/${TOOLCHAIN_PREFIX}-objcopy${BINARY_FILE_EXT})
+#set(CMAKE_OBJDUMP ${CMAKE_FIND_ROOT_PATH}/${TOOLCHAIN_PREFIX}-objdump${BINARY_FILE_EXT})
+
+# Show the size of the final code
+set(CMAKE_SIZE ${CMAKE_FIND_ROOT_PATH}/${TOOLCHAIN_PREFIX}-size${BINARY_FILE_EXT})
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM     NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY     ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE     ONLY)
